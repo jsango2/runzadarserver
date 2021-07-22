fbb06a0aad11f66d86b54a707567900e64420bd8
jsango2 <jure.sango@gmail.com>
2021-07-22 10:17:06 +0200

    info@zaboravljenadalmacija.hr credentials

--- server/server.js
+++ server/server.js
@@ -17,14 +17,14 @@ app.use(express.static(buildPath));
 app.use(cors());
 app.use(express.json());
 
-const contactAddress = 'info@zadarnight.run';
+const contactAddress = 'info@zaboravljenadalmacija.hr';
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
 // let testAccount = await nodemailer.createTestAccount();
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-	host: 'mail.runzadar.com',
+	host: 'mail.zaboravljenadalmacija.hr',
 	port: 465,
 	secure: true, // true for 465, false for other ports
 	auth: {
