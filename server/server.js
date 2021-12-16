1eb797da81a709f3be985ae61abd37c8729e7741
jsango2 <jure.sango@gmail.com>
2021-12-16 12:27:49 +0100

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require("path");
 // const bodyParser = require('body-parser');
-const express = require('express');
-var cors = require('cors');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const express = require("express");
+var cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
@@ -12,68 +12,69 @@ const PORT = process.env.PORT;
 // 		extended: true,
 // 	})
 // );
-const buildPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
 app.use(cors());
 app.use(express.json());
 
-const contactAddress = 'info@zaboravljenadalmacija.hr';
+const contactAddress = "info@zaboravljenadalmacija.hr";
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
 // let testAccount = await nodemailer.createTestAccount();
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-	host: 'mail.zaboravljenadalmacija.hr',
-	port: 465,
-	secure: true, // true for 465, false for other ports
-	auth: {
-		user: process.env.USER, // generated ethereal user
-		pass: process.env.PASS, // generated ethereal password
-	},
+  host: "info@runzadar.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.USER, // generated ethereal user
+    pass: process.env.PASS, // generated ethereal password
+  },
 });
-app.get('/get', (req, res) => {
-	res.send('hello world');
+app.get("/get", (req, res) => {
+  res.send("hello world");
 });
 // send mail with defined transport object
-app.post('/send', (req, res) => {
-	try {
-		const mailOptions = {
-			from: req.body.email,
-			to: contactAddress,
-			subject: 'Zaboravljena Dalmacija Kontakt forma',
-			html: `
-			<h3>ZaboravljenaDalmacija.hr</h3>
+app.post("/send", (req, res) => {
+  try {
+    const mailOptions = {
+      from: req.body.email,
+      to: contactAddress,
+      subject: "RunZadar Prijava via Kontakt forma",
+      html: `
+			<h3>RunZadar.com</h3>
 			  <p>Imate novu poruku od ${req.body.Ime}.</p>
 			  <h4>Detalji kontakta:</h4>
 			  <ul>
 				  <li>Ime pošiljatelja: ${req.body.Ime}</li>
-				  <li>Email: ${req.body.mail}</li>
-				  <li>Poruka: ${req.body.poruka}</li>
+				  <li>Prezime: ${req.body.Prezime}</li>
+				  <li>Datum: ${req.body.Datum}</li>
+				  <li>Email: ${req.body.Email}</li>
 			  </ul>
 			  `,
-		};
+    };
 
-		transporter.sendMail(mailOptions, function (err, info) {
-			if (err) {
-				res.status(500).send({
-					success: false,
-					message: 'Something went wrong. Try again later.',
-				});
-				console.error(err);
-			} else {
-				res.send({
-					success: true,
-					message: 'Thanks for contacting us! We will get back to you ASAP!',
-				});
-			}
-		});
-	} catch (error) {
-		res.status(500).send({
-			success: false,
-			message: 'Something went wrong. Try again later.',
-		});
-	}
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        res.status(500).send({
+          success: false,
+          message: "Something went wrong. Try again later.",
+        });
+        console.error(err);
+      } else {
+        res.send({
+          success: true,
+          message: "Thanks for contacting us! We will get back to you ASAP!",
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong. Try again later.",
+    });
+  }
 });
 
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
