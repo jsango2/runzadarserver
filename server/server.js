4d77ba53ca4f7168d342a01f2d51f6b2fdfe31f9
jsango2 <jure.sango@gmail.com>
2021-12-16 23:39:35 +0100

    initial commit

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const contactAddress = "info@runzadar.com";
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-  host: "info@runzadar.com",
+  host: "prijava@runzadar.com",
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
